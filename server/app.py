ddc9626b13e125c5c5239bdb4165259c7aadd7e5
MDluffy <chinmay81098@gmail.com>
2021-01-11 01:44:02 +0530

    file upload fix

--- server/app.py
+++ server/app.py
@@ -4,7 +4,6 @@ from preprocess import audio_spec, spec_audio, audio_files_to_numpy
 from model import model_out
 from werkzeug.utils import secure_filename
 
-raw_audio = []
 
 DEBUG = True
 
@@ -14,45 +13,21 @@ app.config.from_object(__name__)
 @app.route("/", methods = ["GET"])
 def send_response():
     return "1"
-@app.route("/sendaudio", methods = ["GET"])
+@app.route("/get_clean_audio", methods = ["GET"])
 def send_audio():
-    print(type(raw_audio))
+    raw_audio = audio_files_to_numpy(['../localDB/uploadedAudio/file.wav'], 8000, 8064, 8064, 1.0)
     amp_spec, phase_spec = audio_spec(raw_audio)
     model_output = model_out(amp_spec)
     clean_audio = spec_audio(model_output, phase_spec)
     return clean_audio
 
-@app.route("/recieveaudio", methods = ["GET", "POST"])
+@app.route("/upload_audio", methods = ["GET", "POST"])
 def recieve_audio():
-    global raw_audio
-    '''
     if request.method == "POST":
-        f = open('./file.wav', 'wb')
-        f.write(request.get_data("audio_data"))
-        f.close()    
-    '''
-    if request.method == "POST":
-        print("FORM DATA RECEIVED")
-
-        if "file" not in request.files:
-            print(0)
-            return redirect(request.url)
-
-        file = request.files["file"]
-        if file.filename == "":
-            return redirect(request.url)
-            
-        if file:
-            print(file)
-            recognizer = sr.Recognizer()
-            audioFile = sr.AudioFile(file)
-            with audioFile as source:
-                data = recognizer.record(source)
-            transcript = recognizer.recognize_google(data, key=None)
-            print(transcript)
-
-        
-    return raw_audio
+        f = request.files['audio_data']
+        with open('../localDB/uploadedAudio/file.wav', 'wb') as audio:
+            f.save(audio)
+        return 'success'
 
 
 
